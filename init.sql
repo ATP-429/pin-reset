36b39450e244908845b1073682f434f992395d78
ATP_429 <atpgaming123@gmail.com>
2022-01-01 15:15:13 +0530

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE pin_reset;
 USE pin_reset;
-CREATE TABLE user_info(email varchar(255), password varchar(255), verification_id int);
+CREATE TABLE user_info(username varchar(255), email varchar(255), password varchar(255), verification_id int);
